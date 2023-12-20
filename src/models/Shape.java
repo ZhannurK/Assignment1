1f60974d408c5437f40395ec988defb60ff2eb6c
Zhannur Kainedin <230030@astanait.edu.kz>
2023-12-20 08:01:34 +0600

    blablabla

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -1,12 +1,11 @@
 package models;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class Shape {
-    private List<Point> points;
+    private ArrayList<Point> points;
 
-    // Constructor
     public Shape() {
         this.points = new ArrayList<>();
     }
@@ -18,11 +17,6 @@ public class Shape {
 
     // Calculate the perimeter of the shape
     public double calculatePerimeter() {
-        if (points.size() < 2) {
-            // Not enough points to form a shape
-            return 0.0;
-        }
-
         double perimeter = 0.0;
         for (int i = 0; i < points.size() - 1; i++) {
             perimeter += points.get(i).distance(points.get(i + 1));
@@ -41,14 +35,15 @@ public class Shape {
             return 0.0;
         }
 
-        double longestDistance = 0.0;
+        double longestDistance = points.get(points.size() - 1).distance(points.get(0));
         for (int i = 0; i < points.size() - 1; i++) {
-            for (int j = i + 1; j < points.size(); j++) {
-                double distance = points.get(i).distance(points.get(j));
-                longestDistance = Math.max(longestDistance, distance);
-            }
+            double distance = points.get(i).distance(points.get(i+1));
+            longestDistance = Math.max(longestDistance, distance);
         }
 
         return longestDistance;
+    }
+    public double getAvarageDistance() {
+        return calculatePerimeter()/points.size();
     }
 }
